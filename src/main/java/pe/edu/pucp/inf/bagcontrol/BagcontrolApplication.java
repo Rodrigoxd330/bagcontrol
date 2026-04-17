0fa8c7a7de6ef5b614e86f11f22878ffeeb2a175
Rodrigoxd330 <166078608+Rodrigoxd330@users.noreply.github.com>
2026-04-17 18:13:38 -0500

    first commit

---
+++ src/main/java/pe/edu/pucp/inf/bagcontrol/BagcontrolApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.inf.bagcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/pucp/inf/bagcontrol/BagcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.inf.bagcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
